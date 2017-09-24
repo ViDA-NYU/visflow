613a05b69574abf422e0592b466b4364e06c7983
Jorge Henrique Piazentin Ono <jorgehpo@nyu.edu>
2017-09-24 15:32:18 -0400

    updating initialization script

--- start-script.sh
+++ start-script.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+
+# Initializing mysql
 service mysql start
-cd /var/www/html/server
-mysql -u root "-proot" < init-db.sql
-chmod +x init.sh
-./init.sh
+
+if [ ! -f "/INITIALIZED_VOLUME.flag" ]; then
+  # Control will enter here if INITIALIZED_VOLUME.flag does not exists.
+  echo initializing container
+  touch /INITIALIZED_VOLUME.flag
+  cd /var/www/html/server
+  mysql -u root "-proot" < init-db.sql
+  chmod +x init.sh
+  ./init.sh
+  chown -R www-data:www-data /data
+fi
+
+# Initializing Apache
 /usr/sbin/apache2ctl -D FOREGROUND
