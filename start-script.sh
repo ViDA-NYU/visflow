44187feba3f99fa252de3cbca3d4c02d4ddc9666
Jorge Henrique Piazentin Ono <jorgehpo@nyu.edu>
2017-09-24 16:10:53 -0400

    Saving state in sepparate volume

--- start-script.sh
+++ start-script.sh
@@ -3,15 +3,15 @@
 # Initializing mysql
 service mysql start
 
-if [ ! -f "/INITIALIZED_VOLUME.flag" ]; then
-  # Control will enter here if INITIALIZED_VOLUME.flag does not exists.
-  echo initializing container
-  touch /INITIALIZED_VOLUME.flag
-  cd /var/www/html/server
-  mysql -u root "-proot" < init-db.sql
-  chmod +x init.sh
-  ./init.sh
-  chown -R www-data:www-data /data
+if [ ! -f "/data/visflow/INITIALIZED_VOLUME.flag" ]; then
+# Control will enter here if INITIALIZED_VOLUME.flag does not exists.
+echo initializing container
+touch /data/visflow/INITIALIZED_VOLUME.flag
+cd /var/www/html/server
+mysql -u root "-proot" < init-db.sql
+chmod +x init.sh
+./init.sh
+chown -R www-data:www-data /data
 fi
 
 # Initializing Apache
