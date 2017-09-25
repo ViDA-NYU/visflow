9d30c9969f555dd80418fbdfa7fb09b6eece46ec
Jorge Henrique Piazentin Ono <jorgehpo@nyu.edu>
2017-09-25 15:04:15 -0400

    minor fixes

--- server/GRPC_TA2_TA3/run.sh
+++ server/GRPC_TA2_TA3/run.sh
@@ -8,11 +8,12 @@ if [ ! -e ta3ta2_api ]; then
   git checkout master
   cd ..
   mv ta3ta2-api ta3ta2_api
+  rm -rf ta3ta2_api/.git
 fi
 
 ./update-api-proto.sh
 
-echo "Starting server ..."
-
-# this might need to be in the background
-python TA3.py
+# echo "Starting server ..."
+# 
+# # this might need to be in the background
+# python TA3.py
