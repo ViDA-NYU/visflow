aa4e20c63673b5a0a693b1d167a6ad0784916e6f
Jorge Henrique Piazentin Ono <jorgehpo@nyu.edu>
2017-09-27 10:57:41 -0400

    fixing start-script

--- start-script.sh
+++ start-script.sh
@@ -5,13 +5,15 @@
 service mysql start
 
 if [ ! -f "/data/visflow/INITIALIZED_VOLUME.flag" ]; then
-# Control will enter here if INITIALIZED_VOLUME.flag does not exists.
-echo Initializing container...
-touch /data/visflow/INITIALIZED_VOLUME.flag
-cd /var/www/html/server
-mysql -u root "-proot" < init-db.sql
-./init.sh
-chown -R www-data:www-data /data
+  # Control will enter here if INITIALIZED_VOLUME.flag does not exists.
+  echo Initializing container...
+  touch /data/visflow/INITIALIZED_VOLUME.flag
+  pushd .
+  cd /var/www/html/server
+  mysql -u root "-proot" < init-db.sql
+  ./init.sh
+  chown -R www-data:www-data /data
+  popd
 fi
 
 # Initializing python relay server
